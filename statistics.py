a587ef3cdc3f5bea6af7cd86adf9452f392a9ee3
tidiane-camaret <63314145+tidiane-camaret@users.noreply.github.com>
2021-12-21 02:03:14 +0100

    lexicon modifications

--- statistics.py
+++ statistics.py
@@ -1,11 +1,11 @@
 import pickle
 import matplotlib.pyplot as plt
 
-with open("models_data/run_results.pkl", 'rb') as f:
+with open("models_data/run_results20_12_2.pkl", 'rb') as f:
     stats = pickle.load(f)
 max_score = max([row[6] for row in stats])
 print(max_score)
-stats = stats[500:]
+#stats = stats[500:]
 g = [row[2] for row in stats]
 d = [row[3] for row in stats]
 s = [row[6]/30 for row in stats]
@@ -17,6 +17,6 @@ plt.plot(g, label="Gen")
 plt.plot(d, label="Disc")
 plt.plot(s, label="score")
 plt.legend()
-plt.xlim([30900, 41000])
+#plt.xlim([30900, 41000])
 plt.show()
 
